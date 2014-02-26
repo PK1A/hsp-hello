a8926c58759c7a9d409795d85ea98669ffab2342
pk1a <pkozlowski@amadeus.com>
2014-02-26 15:22:22 +0100

    #space hello world

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+  // Automatically load all the grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    'hsp-compiler': {
+      all: {
+        src: ['**/*.hsp'],
+        dest: 'dist',
+        ext: '.hsp.js',
+        cwd: 'src/',
+        expand: true
+      }
+    },
+    copy: {
+      html: {
+        files: [
+          {expand: true, cwd: 'src/', src: ['**/*.html'], dest: 'dist'}
+        ]
+      }
+    },
+    watch: {
+      all: {
+        files: ['src/**'],
+        tasks: ['default']
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: 'dist',
+          keepalive: true
+        }
+      }
+    }
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', ['hsp-compiler', 'copy']);
+};
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-hsp-hello
+Hello, #space!
 =========
+
+This is a simple project illustrating Grunt setup for a sample #space project.
+It contains configuration of Grunt tasks for:
+* compiling #space templates:
+    * `grunt` - one-time compilation
+    * `grunt watch` - continuous compilation on each file change
+* running a simple HTTP server: `grunt connect`
+
+== Install
+
+Clone this repository and then run:
+* `npm install grunt-cli -g`
+* `npm install`
+
+== Play
+
+Open 2 terminal windows with the following commands:
+* `grunt watch`
+* `grunt connect`
+and point your browser to http://localhost:8000/
